d0be3d79572b0f225b26d0053a466690c54f182b
snkeegan <sarah@fenyolab.org>
2023-08-02 15:19:19 -0400

    Added origin finding script, filtering script, and origin files for RPE

---
+++ find_origins.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 12 18:47:39 2017
+
+@author: sarahkeegan, davidfenyo
+
+this program will use the derivative of smoothed watson and crick strand signal to find origins based on okazaki
+sequencing data: DER-SCORE = -1 * DER(W) * DER(C), where DER-SCORE > 0, an origin region is identified
+
+input is tab delimited text files with position and watson and crick signals per 1kb
+
+output is txt file with origin locations, origin midpoint
+
+"""
+
+import pandas as pd
+import numpy as np
+import sys
+import os
+
+if len(sys.argv) >= 3:
+    filename = sys.argv[1]          # input file
+    output_dir = sys.argv[2]        # output directory
+else:
+    print("Missing command line input.  Attempting to run with default settings.")
+    filename = "/Users/snk218/Dropbox (NYU Langone Health)/mac_files/fenyolab/data_and_results/huang/raw_files/rpe_edu_2.txt"
+    output_dir = "/Users/snk218/Dropbox (NYU Langone Health)/mac_files/fenyolab/data_and_results/huang/raw_files/results/origins"
+
+############################
+
+window = 'hanning'
+width = 60  # 80
+
+if window in ['hanning', 'hamming', 'bartlett', 'blackman']:
+    w = eval('np.'+window+'(2*width+1)')
+else:
+    window = 'flat'
+    w = np.ones(2*width+1)
+
+peaks = {}
+peaks_width = {}
+
+print(f" {filename}: Finding origins...")
+
+# read file contents into pandas DataFrame object, input columns: chr, pos, w, c
+# each row is the signal values averaged over 1Kb
+df = pd.read_table(filename, low_memory=False)
+
+# add rfd
+df['rfd']=df['c']-df['w']/(df['c']+df['w'])
+df['rfd']=np.nan_to_num(df['rfd'])
+
+# add perc_c
+df['perc_c']=df['c']/(df['w']+df['c'])
+df['perc_c']=np.nan_to_num(df['perc_c'])
+
+# convolves values from c and w columns with the window function (for smoothing)
+df['c_conv']=np.convolve(w/w.sum(),df['c'],'same')
+df['w_conv']=np.convolve(w/w.sum(),df['w'],'same')
+
+# calculate the 'derivative' of the crick/watson smoothed signal at each point
+# the diff() function calculates the first order difference, i.e. for each point, diff[i] = x[i+1]-x[i]
+# (this is the change in x, and the change in y is always 1)
+df['c_diff']=df['c_conv'].diff()
+df['w_diff']=df['w_conv'].diff()
+
+# at initiation sites, crick should be increasing so if it is decreasing set to 0
+df.loc[df['c_diff']<0,'c_diff']=0
+
+# at initiation sites, watson should be decreasing so if it is increasing set to 0
+df.loc[df['w_diff']>0, 'w_diff']=0
+
+# calculate our 'Score' by multipying c and w derivatives (add minus sign to make it positive)
+df['cw_diff_prod']=-1*df['c_diff']*df['w_diff']
+df.loc[df['cw_diff_prod'] == -0, 'cw_diff_prod'] = 0
+
+# loop through each 1kb position and look for peaks by finding max cw_diff_prod between zeros values
+df.index = range(len(df))
+df.fillna(0, inplace=True)
+height = 0        # stores running max score found for current peak
+height_idx = 0    # index of the current max score (cw_diff_prod)
+start_idx = 0     # start index of the current 'peak'
+previous_idx = 0
+
+for idx in df.index:
+    if idx > 0 and idx % 100000 == 0:
+        print(idx)
+
+    # idx is the index position in the DataFrame
+    if df['cw_diff_prod'].iloc[idx] > 0:
+        # if Score greater than 0, we are in a peak, continue to find max
+        if height == 0: # at the beginning of a peak
+            start_idx = previous_idx
+        if height < df['cw_diff_prod'].iloc[idx]:
+            # looking for max height in the peak
+            height = df['cw_diff_prod'].iloc[idx]
+            height_idx = idx
+
+    else:  # if Score is 0, we are between peaks or at the end of a peak
+        if height > 0:  # height has a value, we are at the end of a peak
+            # store the index of the max height; the width is the start/end of the 0-values on either side of the peak
+            peaks[(df['chr'].iloc[height_idx],
+                   df['pos'].iloc[height_idx])] = height
+            peaks_width[(df['chr'].iloc[height_idx],
+                         df['pos'].iloc[height_idx])] = (df['pos'].iloc[start_idx],
+                                                         df['pos'].iloc[idx])
+
+        height = 0
+        height_idx = 0
+        start_idx = 0
+
+    previous_idx = idx
+
+# save origin locations and scores to output file
+print("Saving origin locations...")
+df_to_save = pd.DataFrame()
+for chr_, peak in peaks:
+    (start_pos1, end_pos1) = peaks_width[(chr_, peak)]
+    df_to_save = df_to_save.append(dict(chr=chr_,
+                                        pos=peak,
+                                        height=peaks[(chr_, peak)],
+                                        start=start_pos1,
+                                        end=end_pos1,
+                                        ), ignore_index=True)
+fileroot = os.path.splitext(os.path.split(filename)[1])[0]
+df_to_save.to_csv(output_dir + '/' + fileroot + '-' + window + str(width) + '-origins.txt', sep='\t')
+
---
+++ origins_norm.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats
+import seaborn as sns
+from scipy.stats import gaussian_kde
+import os
+
+if len(sys.argv) >= 3:
+    filename = sys.argv[1]  # input file
+    output_dir = sys.argv[2]  # output directory
+else:
+    print("Missing command line input.  Attempting to run with default settings.")
+    filename = "/Users/snk218/Dropbox (NYU Langone Health)/mac_files/fenyolab/data_and_results/huang/raw_files/results/origins/rpe_edu_2-hanning60-origins.txt"
+    output_dir = "/Users/snk218/Dropbox (NYU Langone Health)/mac_files/fenyolab/data_and_results/huang/raw_files/results/origins"
+
+# read origins file
+df = pd.read_csv(filename, sep='\t', low_memory=False, index_col=0)
+print(len(df))
+
+fileroot = os.path.splitext(os.path.split(filename)[1])[0]
+
+id = []
+chr_coor = []
+heights = []
+bins = []
+
+# log normalize DER score (height) to view data distribution
+df['height'] = scipy.stats.boxcox(df['height'], lmbda=0.1, alpha=None)
+df['height-log'] = np.log10(df['height'])
+df['height-log'] = (df['height'])
+
+# snsplot of normalized origins
+#sns.kdeplot(df['height-log'])
+#plt.savefig(output_dir + '/' + fileroot + '-kde.png')
+
+# calculate kde gaussian without plot to find fwhm
+kde = gaussian_kde(df['height-log'])
+x = np.linspace(df['height-log'].min(), df['height-log'].max(), 14137) #len(df))
+y = kde(x)
+
+plt.plot(x, y, color='black')
+
+halfmax = y.max()/2
+maxpos = y.argmax()
+leftpos = (np.abs(y[:maxpos] - halfmax)).argmin()
+rightpos = (np.abs(y[maxpos:] - halfmax)).argmin() + maxpos
+fullwidthhalfmax = x[rightpos] - x[leftpos]
+print(x[leftpos])
+
+plt.axvline(x[leftpos])
+plt.axvline(x[rightpos])
+plt.savefig(output_dir + '/' + fileroot + '-kde.png')
+plt.clf()
+
+# use position of max height to define origin +/-10kb on either side
+df['pos-up'] = df['pos'] + 10000
+df['pos-down'] = df['pos'] - 10000
+
+df_ = df[(df['height-log'] >= x[leftpos]) & (df['height-log'] <= x[rightpos])]
+print(len(df_))
+
+# calculate kde gaussian without plot to find fwhm
+kde = gaussian_kde(df_['height-log'])
+x = np.linspace(df_['height-log'].min(), df_['height-log'].max(), len(df_))
+y = kde(x)
+plt.plot(x, y, color='black')
+plt.savefig(output_dir + '/' + fileroot + '-kde-filtered.png')
+plt.clf()
+
+df_.index = range(len(df_))
+df_.to_csv(output_dir + '/' + fileroot + '-log10-filtered.txt', sep='\t', index=True)
+
+
